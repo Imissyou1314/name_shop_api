bafb708bbdf4ab0123eee098f335461ef73a7928
missGo <1255418233@qq.com>
2022-12-01 02:52:44 +0800

    update tools desc

--- utils/tools.go
+++ utils/tools.go
@@ -1,5 +1,6 @@
 package utils
 
+// 数组过滤
 func Filter[T any](slice []T, f func(T) bool) []T {
 	var n []T
 	for _, e := range slice {
@@ -10,6 +11,7 @@ func Filter[T any](slice []T, f func(T) bool) []T {
 	return n
 }
 
+// 过滤重复的字符
 func RemoveDuplicateEle(addrs []string) []string {
 	result := make([]string, 0, len(addrs))
 	temp := map[string]struct{}{}
